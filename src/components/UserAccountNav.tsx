7ae602f7e7a3b5d164a82f3dc681d61b60ffb289
TrevorBaba <trevor.baba@berkeley.edu>
2023-08-01 01:46:54 -0700

    Add light dark mode and user avatar

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -11,6 +11,8 @@ import {
 import Link from "next/link";
 import { signOut } from "next-auth/react";
 import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import UserAvatar from "./UserAvatar";
 
 type Props = {
   user: Pick<User, "name" | "image" | "email">;
@@ -21,7 +23,7 @@ const UserAccountNav = ({ user }: Props) => {
     <DropdownMenu>
       <DropdownMenuTrigger>
         {/* user avatar */}
-        <Button>hi</Button>
+        <UserAvatar user={user} />
       </DropdownMenuTrigger>
       <DropdownMenuContent className="bg-white" align="end">
         <div className="flex items-center justify-start gap-2 p-2">
@@ -44,8 +46,10 @@ const UserAccountNav = ({ user }: Props) => {
             e.preventDefault();
             signOut().catch(console.error);
           }}
+          className="text-red-600 cursor-pointer"
         >
           Sign Out
+          <LogOut className="w-4 h-4 ml-2" />
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
